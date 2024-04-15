92e9409ad7a6cbe6961bd7620d6a4373d51474de
David Fry <david.fry@ivcevidensia.com>
2024-04-15 16:34:16 +0100

    simple change

---
+++ test-pacman-api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_pacman_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
